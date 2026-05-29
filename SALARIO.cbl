1b61ad306635a714ec14ebc4c833bfaf07921bbd
VITOR LEITE SETRAGNI <165705231+VitorSetragni@users.noreply.github.com>
2026-05-29 11:45:13 -0300

    Add files via upload

--- SALARIO.cbl
+++ SALARIO.cbl
@@ -11,12 +11,16 @@
        WORKING-STORAGE SECTION.
       *
        01 FUNCIONARIO.
-            05 NOME             pic x(25).
-            05 SALARIO          pic 9(9)V99.
-            05 TEMPO-DE-EMPRESA pic 99.
-       01 OPCAO                 PIC 9 VALUE 1.
-       01 SALARIO-FINAL         PIC 9(9)V99.
-       01 BONUS                 PIC 9(6)V99.
+            05 NOME               pic x(25).
+            05 SALARIO            pic 9(9)V99.
+            05 TEMPO-DE-EMPRESA   pic 99.
+       01 OPCAO                   PIC 9 VALUE 1.
+       01 SALARIO-FINAL           PIC 9(9)V99.
+       01 BONUS                   PIC 9(6)V99.
+       01 SALARIO-SEM-DECIMAL     pic 9(11).
+       01 SALARIO-IMPRESSAO       PIC ZZZZZZZZZZZ9.99.
+       01 SALARIO-FINAL-IMPRESSAO PIC ZZZZZZZZZZZ9.99.
+       01 BONUS-IMPRESSAO         PIC ZZZZZZZ9.99.
       *
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
@@ -40,14 +44,15 @@
                PERFORM EXIBE-RESULTADO.
 
        ENTRADA-DADOS.
-            DISPLAY "Digite o seu nome"
+            DISPLAY "Digite o seu nome".
             ACCEPT NOME.
-            DISPLAY "Digite seu salario sem utilizar a virgula ou ponto"
+            DISPLAY "Digite seu salario sem utilizar a virgula".
             DISPLAY "Você deve escrever seu salario no formato XXXXXYY "
-      -     "inves de XXXXXX,YY"
-            ACCEPT SALARIO.
+      -     "inves de XXXXXX,YY".
+            ACCEPT SALARIO-SEM-DECIMAL.
+            COMPUTE SALARIO = SALARIO-SEM-DECIMAL / 100.
             DISPLAY "Digite a quantidade de anos que voce esta na "
-      -     "empresa"
+      -     "empresa".
             ACCEPT TEMPO-DE-EMPRESA.
       *
        CALCULA-BONUS.
@@ -63,10 +68,14 @@
            COMPUTE SALARIO-FINAL = SALARIO + BONUS.
       *
        EXIBE-RESULTADO.
+           MOVE SALARIO TO SALARIO-IMPRESSAO.
+           MOVE BONUS TO BONUS-IMPRESSAO.
+           MOVE SALARIO-FINAL TO SALARIO-FINAL-IMPRESSAO.
+      *
            DISPLAY " ".
            DISPLAY "NOME:" NOME.
-           DISPLAY "SALARIO BASE:" SALARIO.
-           DISPLAY "BONUS:" BONUS.
-           DISPLAY "SALARIO FINAL" SALARIO-FINAL.
+           DISPLAY "SALARIO BASE:" SALARIO-IMPRESSAO.
+           DISPLAY "BONUS:" BONUS-IMPRESSAO.
+           DISPLAY "SALARIO FINAL" SALARIO-FINAL-IMPRESSAO.
       *
        END PROGRAM SALARIO.
